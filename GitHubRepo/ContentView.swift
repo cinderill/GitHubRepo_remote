8cd5f3c56ca0c5fcce410cc96676505a15838170
Jamil Salimgareev <cinderilll@icloud.com>
2023-09-20 09:08:57 +0300

    Initial Commit

---
+++ GitHubRepo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHubRepo
+//
+//  Created by Жамиль Салимгареев on 20.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("This is a test")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubRepo/GitHubRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepoApp.swift
+//  GitHubRepo
+//
+//  Created by Жамиль Салимгареев on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
